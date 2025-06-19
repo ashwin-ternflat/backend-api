501b9599603d4f4145c0c820498909cc8c275c10
Ashwin <ashwinbala2107@gmail.com>
2025-06-19 12:28:53 +0530

    CI pipeline

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,9 @@
+import os
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     app_name: str = "Your App"
-    mongo_uri: str
+    mongo_uri: str = os.getenv("MONGO_URI", "mongodb://localhost:27017/backend_api_db")
     mongo_db_name: str 
     class Config:
         env_file = ".env"
